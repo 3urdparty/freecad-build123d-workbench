afc01568423ff4b3489b32132f99411d525f78da
jobi <git@jobi.is>
2026-08-09 16:59:07 +0100

    chore(release): prepare version 0.2.0

--- kernel/fc_code_kernel/__init__.py
+++ kernel/fc_code_kernel/__init__.py
@@ -4,4 +4,4 @@ Runs in its own virtual environment with OCP/build123d/cadquery. Must never
 import FreeCAD. Geometry leaves this process only as serialized BREP.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
